803c64dfda3d24e3b24c8f0e240f2999d8ce5dd4
carvalhobe4 <beatriz.costa220@etec.sp.gov.br>
2025-02-25 18:18:08 -0300

    funcoes node

---
+++ aula2/index.js
@@ -0,0 +1,15 @@
+function soma(n1, n2){
+    return (n1+n2)
+}
+function sub(n1, n2){
+    return (n1-n2)
+}
+function mult(n1, n2){
+    return (n1*n2)
+}
+function div(n1, n2){
+    return (n1/n2)
+}
+console.log(soma(13,63))
+
+console.log('Soma deu ${} ')
